e57055493460bd90e8085f91e312c42099dea7e2
navin <navin@ThinkPad>
2021-12-19 21:38:26 +0530

    s

--- MavenProject/src/main/java/tests/rest.java
+++ MavenProject/src/main/java/tests/rest.java
@@ -3,7 +3,6 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
-import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
@@ -22,7 +21,7 @@ public class rest {
 
 		// Make a request to the server by specifying the method Type and the method URL.
 		// This will return the Response from the server. Store the response in a variable.
-		Response response = httpRequest.get("/Hyderabad");
+		Response response = httpRequest.get("/Amrits");
 
 		// Now let us print the body of the message to see what response
 		// we have received from the server
